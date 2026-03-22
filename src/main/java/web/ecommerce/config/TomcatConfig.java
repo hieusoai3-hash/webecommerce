e4d2175a6d861f6922a9d7e20acd097a0cd82c4c
Lợi Nguyễn <91263864+Chiseled141@users.noreply.github.com>
2026-03-22 13:34:32 +0700

    docker added

---
+++ src/main/java/web/ecommerce/config/TomcatConfig.java
@@ -0,0 +1,19 @@
+package web.ecommerce.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+        return factory -> factory.addConnectorCustomizers((Connector connector) -> {
+            connector.setMaxPostSize(-1);
+            connector.setMaxSavePostSize(-1);
+        });
+    }
+}
